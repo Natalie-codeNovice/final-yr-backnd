4f6bab04799284746de5830e30bd69f527364173
Natalie-codeNovice <152331814+Natalie-codeNovice@users.noreply.github.com>
2024-08-21 10:01:53 +0200

    incomes and pagination

--- controller/incomeController.js
+++ controller/incomeController.js
@@ -3,29 +3,33 @@ const db = require('../models');
 // Get all income and total income sum for a user
 const getIncome = async (req, res) => {
   const userId = req.params.id;
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 10;
+  const offset = (page - 1) * pageSize;
 
   try {
-    // Fetch all income transactions
+    // Fetch income transactions with pagination
     const income = await db.transactions.findAll({ 
-      where: { userId, type: 'income' }
+      where: { userId, type: 'income' },
+      limit: pageSize,
+      offset: offset
     });
 
     // Calculate the total sum of income
     const totalIncome = income.reduce((sum, transaction) => sum + parseFloat(transaction.amount), 0);
-
-    // Format totalIncome as a currency string
     const formattedTotalIncome = totalIncome.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
     if (income.length === 0) {
       return res.status(404).json({ message: 'No income found', totalIncome: formattedTotalIncome });
     }
 
-    res.status(200).json({ income, totalIncome: formattedTotalIncome });
+    res.status(200).json({ income, totalIncome: formattedTotalIncome, page, pageSize });
   } catch (error) {
     console.error('Error retrieving income:', error);
     res.status(500).json({ message: 'Error retrieving income', error: error.message || error });
   }
 };
+
 
 module.exports = {
   getIncome
