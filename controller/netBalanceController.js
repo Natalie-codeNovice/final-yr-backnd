d937f46b7f2a2bb4b24784ddddf426aa123c1f1c
Natalie-codeNovice <anathaliemukash@gmail.com>
2024-08-22 11:06:48 +0200

    prof

--- controller/netBalanceController.js
+++ controller/netBalanceController.js
@@ -4,17 +4,18 @@ const db = require('../models');
 const getNetBalance = async (req, res) => {
   try {
     const userId = req.user.userId;
-    const netBalance = await db.netBalances.findOne({ where: { userId } });
+    let netBalance = await db.netBalances.findOne({ where: { userId } });
+
     if (!netBalance) {
-      return res.status(404).json({ message: 'Net balance not found' });
+      netBalance = { balance: 0.0 }; // Set netBalance to an object with a balance property
     }
+
     res.status(200).json(netBalance);
   } catch (error) {
     console.error('Error retrieving net balance:', error);
     res.status(500).json({ message: 'Error retrieving net balance', error: error.message || error });
   }
 };
-
 
 module.exports = {
   getNetBalance
